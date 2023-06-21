4b7df67756996d352df83e8cbacc728fa1ddc93b
admin <admin@MacBook-Pro-admin.local>
2023-06-21 17:04:11 +0300

    creating project structure

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,4 +1,8 @@
 import pytest
-class TestFooter
+import pages
+import time
+
+class TestFooter:
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
-        pass
+        pages.index_page.open_index_page(page)
+        time.sleep(10)
