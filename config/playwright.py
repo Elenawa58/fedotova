fe38c39d3c99f73ee2fd9cd0d5ab1abd4243760d
admin <admin@MacBook-Pro-admin.local>
2023-06-21 15:38:53 +0300

    creating project structure

--- config/playwright.py
+++ config/playwright.py
@@ -1,7 +1,8 @@
 import os
 
+
 class Playwright:
-    PAGE_VIEWPORT_SIZE = {'width':1920, 'height':1080}
+    PAGE_VIEWPORT_SIZE = {'width': 1920, 'height': 1080}
     ENV = os.getenv('ENV') if os.getenv('ENV') is not None else 'chrome'
     BROWSER = os.getenv('BROWSER') if os.getenv('BROWSER') is not None else 'chrome'
     IS_HEADLESS = os.getenv('HEADLESS') if os.getenv('HEADLESS') is not None else False
